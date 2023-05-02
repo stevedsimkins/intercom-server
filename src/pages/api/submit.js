253e00e3652c55ae4fda8ec5fc9a0b1f4f087c28
Steve <stevedsimkins@gmail.com>
2023-05-02 16:53:05 -0400

    testing conversation reply

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -1,14 +1,32 @@
-export default function handler(req, res) {
+import FormData from "form-data"
+
+export default async function handler(req, res) {
   console.log(req.body)
   const reqBody = JSON.stringify(req.body.conversation.source.body)
+  const convoId = JSON.stringify(req.body.conversation.id)
+  const formData = new FormData()
 
-  // API call to content moderation goes here 
+  const convoData = JSON.stringify({
+    type: "admin",
+    message_type: "comment",
+    body: "Hey there!",
+    admin_id: req.body.admin.id
+  })
+  formData.append(convoData)
+
+  const conversationRes = await fetch(`https://api.intercom.io/conversations/${convoId}/reply`, formData, {
+    headers: {
+      "Authorization": `Bearer ${process.env.INTERCOM_KEY}`
+    }
+  })
+
+  console.log(conversationRes.data)
 
   res.send({
     canvas: {
       content: {
         components: [
-          { type: "text", text: `${reqBody}`,
+          { type: "text", text: `${conversationRes.data}`,
            style: "header", align: "center" },
         ],
       },

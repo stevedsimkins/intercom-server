fdc9c6b4481fbb000034992f6592125a1d5d686b
Steve <stevedsimkins@gmail.com>
2023-05-02 17:03:45 -0400

    small changes

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -3,12 +3,15 @@ import FormData from "form-data"
 export default async function handler(req, res) {
   const reqBody = JSON.stringify(req.body.conversation.source.body)
   const convoId = JSON.stringify(req.body.conversation.id)
+  const adminId = JSON.stringify(req.boyd.admin.id)
   const formData = new FormData()
+  console.log(convoId)
+  console.log(adminId)
 
   formData.append('type', 'admin')
   formData.append('message_type', 'comment')
   formData.append('body', 'Hey there!')
-  formData.append('admin_id', `${req.body.admin.id}`)
+  formData.append('admin_id', `${adminId}`)
 
   const conversationRes = await fetch(`https://api.intercom.io/conversations/${convoId}/reply`, formData, {
     headers: {

25189ef0c73523b9c8984d8bf7a769e320f729aa
Steve <stevedsimkins@gmail.com>
2023-05-02 21:11:45 -0400

    ooo nice

--- src/pages/api/submit.js
+++ src/pages/api/submit.js
@@ -8,12 +8,13 @@ export default async function handler(req, res) {
   const reqBody = JSON.stringify(req.body.conversation.source.body)
   const convoId = req.body.conversation.id
   const adminId = req.body.admin.id
+  const adminName = req.body.admin.name
 
   try {
     const conversationRes = await sdk.replyConversation({
       message_type: 'comment',
       type: 'admin',
-      body: 'Hey there! Thank you for contacting us, we will reply later!',
+      body: `<p>Hey there!</p><br/><p>Thank you for letting us know, we will keep in touch!</p><br/><br/><p>Best, <br/>${adminName}</p>`,
       admin_id: `${adminId}` }, {id: `${convoId}`, 'intercom-version': '2.8'})
 
     const data = await conversationRes
